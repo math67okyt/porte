b22246596739b53dd2c50bb896c819a14bbd5178
math67okyt <mathieu.issler@gmail.com>
2026-03-02 18:47:30 +0100

    firt commit

---
+++ serrure_electrique.ino
@@ -0,0 +1,67 @@
+#include <SPI.h>
+#include <MFRC522.h>
+
+#define SS_PIN 10
+#define RST_PIN 9
+#define LOCK_PIN 7   // Broche pour relais/servo
+
+MFRC522 rfid(SS_PIN, RST_PIN);
+
+// UID autorisé (à modifier avec ton badge)
+byte authorizedUID[4] = {0xDE, 0xAD, 0xBE, 0xEF};
+
+void setup() {
+  Serial.begin(9600);
+  SPI.begin();
+  rfid.PCD_Init();
+
+  pinMode(LOCK_PIN, OUTPUT);
+  digitalWrite(LOCK_PIN, LOW); // serrure fermée
+
+  Serial.println("Systeme RFID prêt");
+}
+
+void loop() {
+
+  // Attente d'une carte
+  if (!rfid.PICC_IsNewCardPresent()) return;
+  if (!rfid.PICC_ReadCardSerial()) return;
+
+  Serial.print("UID : ");
+
+  // Affichage UID
+  for (byte i = 0; i < rfid.uid.size; i++) {
+    Serial.print(rfid.uid.uidByte[i], HEX);
+    Serial.print(" ");
+  }
+  Serial.println();
+
+  // Vérification UID
+  if (checkUID()) {
+    Serial.println("Accès autorisé");
+    ouvrirSerrure();
+  } else {
+    Serial.println("Accès refusé");
+  }
+
+  rfid.PICC_HaltA();
+}
+
+// Vérifie si badge est autorisé
+bool checkUID() {
+
+  for (byte i = 0; i < 4; i++) {
+    if (rfid.uid.uidByte[i] != authorizedUID[i]) {
+      return false;
+    }
+  }
+  return true;
+}
+
+// Ouvre la serrure 5 secondes
+void ouvrirSerrure() {
+
+  digitalWrite(LOCK_PIN, HIGH); // ouvrir
+  delay(5000);                 // 5 s
+  digitalWrite(LOCK_PIN, LOW);  // fermer
+}
